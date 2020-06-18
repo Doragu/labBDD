5efeb82b0dddcdf4d4f317a35eac08141a3d6d7b
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-06-18 23:18:10 +0200

    feature1_impl added @When implementation

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -2,6 +2,10 @@ package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
 
+import edu.iis.mto.bdd.trains.model.Line;
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
+import edu.iis.mto.bdd.trains.services.TimetableService;
+import org.hamcrest.Matchers;
 import org.joda.time.LocalTime;
 
 import cucumber.api.PendingException;
@@ -10,7 +14,12 @@ import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class OptimalItinerarySteps {
+
+    private Line line;
+    private String destination;
 
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
@@ -27,6 +36,9 @@ public class OptimalItinerarySteps {
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        throw new PendingException();
+        TimetableService timetableService = new InMemoryTimetableService();
+        List<LocalTime> arrivalTimes = timetableService.findArrivalTimes(line, destination);
+
+        assertThat(arrivalTimes, Matchers.is(expectedTrainTimes));
     }
 }
