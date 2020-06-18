18c355dce5c21c16b9bc212bf4aa7c86fe4bc2ff
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-06-19 00:31:31 +0200

    feature1_impl added @Given implementation

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -2,14 +2,12 @@ package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
 
-import edu.iis.mto.bdd.trains.model.Line;
+import edu.iis.mto.bdd.trains.services.BasicIntineraryService;
 import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 import edu.iis.mto.bdd.trains.services.IntineraryService;
-import edu.iis.mto.bdd.trains.services.TimetableService;
-import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.is;
 import org.joda.time.LocalTime;
 
-import cucumber.api.PendingException;
 import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
@@ -25,19 +23,17 @@ public class OptimalItinerarySteps {
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
-        throw new PendingException();
-
+        intineraryService = new BasicIntineraryService(new InMemoryTimetableService(), 15);
     }
 
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
     public void whenIWantToTravel(String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
-        this.arrivalTimes = intineraryService.findNextDepartures(departure, destination, startTime);
+        arrivalTimes = intineraryService.findNextDepartures(departure, destination, startTime);
     }
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-
-        assertThat(arrivalTimes, Matchers.is(expectedTrainTimes));
+        assertThat(arrivalTimes, is(expectedTrainTimes));
     }
 }
